11663702ed3dc41fb35ec802547d61f62b1a6168
Ponja94 <henrique.danielb@gmail.com>
2022-07-11 22:25:12 -0300

    1st commit

--- src/components/Oi.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <p>OI</p>   
-</template>
---
+++ src/views/Pedidos.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="pedidos">
+    <h1>Página de pedidos</h1>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'Pedidos',
+    
+  }
+
+</script>
